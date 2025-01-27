a33638b9269e6e1d8559d1088ce41260cb7a6dfc
Toshiaki Maki <makingx@gmail.com>
2025-01-27 23:02:31 +0900

    Support loading local files and previous reports

--- src/main/java/lol/maki/leading/Scraper.java
+++ src/main/java/lol/maki/leading/Scraper.java
@@ -5,94 +5,138 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Scraper implements CommandLineRunner {
+public class Scraper implements ApplicationRunner {
 
-	static Playwright playwright = Playwright.create();
-
-	static Browser browser = playwright.chromium().launch();
-
-	BrowserContext context;
-
-	Page page;
+	private final ObjectMapper objectMapper;
 
 	private static final Logger log = LoggerFactory.getLogger(Scraper.class);
 
-	public Scraper() {
-		context = browser.newContext();
-		context.setDefaultTimeout(30_000);
-		page = context.newPage();
+	public static final String DIR_OPT = "dir";
+
+	public Scraper(ObjectMapper objectMapper) {
+		this.objectMapper = objectMapper;
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(ApplicationArguments args) throws Exception {
 		log.info("Scraper started");
-		page.navigate("https://www.jra.go.jp/datafile/leading/");
-		page.locator("a:has-text(\"種牡馬\")").click();
-		Pattern pattern = Pattern.compile("(\\d{4})年(\\d{1,2})月(\\d{1,2})日現在");
-		Matcher matcher = pattern.matcher(page.innerHTML("#leading_horse"));
-		if (!matcher.find()) {
+		ArrayNode jsonArray = this.objectMapper.createArrayNode();
+		AtomicReference<LocalDate> date = new AtomicReference<>();
+		if (args.containsOption(DIR_OPT) && !args.getOptionValues(DIR_OPT).isEmpty()) {
+			String dir = args.getOptionValues(DIR_OPT).getFirst();
+			log.info("Loading {}", dir);
+			try (Stream<Path> list = Files.list(Path.of(dir))) {
+				Pattern pattern = Pattern.compile("([0-9]+)\\.html$");
+				list.filter(Files::isRegularFile)
+					.filter(f -> f.getFileName().toString().endsWith(".html"))
+					.sorted(Comparator.comparingInt(path -> {
+						Matcher matcher = pattern.matcher(path.getFileName().toString());
+						return matcher.find() ? Integer.parseInt(matcher.group(1)) : 1;
+					}))
+					.forEachOrdered(file -> {
+						try {
+							log.info("Parsing file {}", file.getFileName());
+							Document doc = Jsoup.parse(file);
+							parseDocument(doc, jsonArray);
+							if (date.get() == null) {
+								targetDate(doc.select("#leading_horse").html()).ifPresent(date::set);
+							}
+						}
+						catch (IOException e) {
+							throw new UncheckedIOException(e.getMessage(), e);
+						}
+					});
+			}
+		}
+		else {
+			Playwright playwright = Playwright.create();
+			Browser browser = playwright.chromium().launch();
+			BrowserContext context = browser.newContext();
+			context.setDefaultTimeout(30_000);
+			try {
+				Page page = context.newPage();
+				page.navigate("https://www.jra.go.jp/datafile/leading/");
+				page.locator("a:has-text(\"種牡馬\")").click();
+				Optional<LocalDate> targetDate = targetDate(page.innerHTML("#leading_horse"));
+				targetDate.ifPresent(date::set);
+				parsePage(page, jsonArray);
+			}
+			finally {
+				context.close();
+				playwright.close();
+			}
+		}
+		if (date.get() == null) {
 			log.error("Unable to find date");
 			return;
+		}
+		String json = this.objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonArray);
+		log.info("output to {}.json", date.get());
+		Files.copy(new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8)), Path.of(date.get() + ".json"),
+				StandardCopyOption.REPLACE_EXISTING);
+	}
+
+	Optional<LocalDate> targetDate(String html) {
+		Pattern pattern = Pattern.compile("(\\d{4})年(\\d{1,2})月(\\d{1,2})日現在");
+		Matcher matcher = pattern.matcher(html);
+		if (!matcher.find()) {
+			return Optional.empty();
 		}
 		int year = Integer.parseInt(matcher.group(1));
 		int month = Integer.parseInt(matcher.group(2));
 		int day = Integer.parseInt(matcher.group(3));
 		LocalDate date = LocalDate.of(year, month, day);
-		ObjectMapper mapper = new ObjectMapper();
-		ArrayNode jsonArray = mapper.createArrayNode();
-		try {
-			parsePage(page, jsonArray, mapper);
-			String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonArray);
-			log.info("output to {}.json", date);
-			Files.copy(new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8)), Path.of(date + ".json"),
-					StandardCopyOption.REPLACE_EXISTING);
-		}
-		finally {
-			context.close();
-			playwright.close();
-		}
+		return Optional.of(date);
 	}
 
-	void parsePage(Page page, ArrayNode jsonArray, ObjectMapper mapper) throws Exception {
+	void parsePage(Page page, ArrayNode jsonArray) throws Exception {
 		log.info("Parsing page [{}]({})", page.title(), page.url());
 		Document doc = Jsoup.parse(page.innerHTML("body"));
-		parseDocument(doc, jsonArray, mapper);
+		parseDocument(doc, jsonArray);
 		if (doc.select("#leading_horse > div.pager_block > div").text().contains("次の20件")) {
-			var next = page.locator("a:has-text(\"次の20件\")");
+			Locator next = page.locator("a:has-text(\"次の20件\")");
 			Thread.sleep(1000);
 			next.click();
-			parsePage(page, jsonArray, mapper);
+			parsePage(page, jsonArray);
 		}
 	}
 
-	void parseDocument(Document doc, ArrayNode jsonArray, ObjectMapper mapper) {
+	void parseDocument(Document doc, ArrayNode jsonArray) {
 		Element table = doc.getElementById("leading_horse").selectFirst("table.basic");
 		Elements rows = table.select("tbody tr");
 		for (Element row : rows) {
 			Elements cols = row.select("td");
 			if (!cols.isEmpty()) {
-				ObjectNode horseData = mapper.createObjectNode();
+				ObjectNode horseData = this.objectMapper.createObjectNode();
 				horseData.put("rank", Integer.valueOf(cols.get(0).text())); // 順位
 				String horseNameAndYear = cols.get(1).text();
 				String[] nameAndYear = horseNameAndYear.split("（|）"); // 「（」と「）」で分割
