a202a10a97074771ec0a8dfe410cfcda4c58c523
Toshiaki Maki <makingx@gmail.com>
2026-03-12 18:20:42 +0900

    Upgrade to Spring Boot 4

--- src/main/java/lol/maki/leading/AbstractJraLeadingSireScrapeTasklet.java
+++ src/main/java/lol/maki/leading/AbstractJraLeadingSireScrapeTasklet.java
@@ -29,10 +29,10 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.StepContribution;
 import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.infrastructure.repeat.RepeatStatus;
 import org.springframework.util.StringUtils;
 
 public abstract class AbstractJraLeadingSireScrapeTasklet implements Tasklet {
--- src/main/java/lol/maki/leading/JobConfig.java
+++ src/main/java/lol/maki/leading/JobConfig.java
@@ -1,28 +1,25 @@
 package lol.maki.leading;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.Job;
 import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.job.parameters.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.Step;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration(proxyBeanMethods = false)
 public class JobConfig {
 
 	@Bean
-	public Step scrapeStep(JobRepository jobRepository, PlatformTransactionManager transactionManager,
-			JraLeadingSireScrapeTasklet tasklet) {
-		return new StepBuilder("Scrape", jobRepository).tasklet(tasklet, transactionManager).build();
+	public Step scrapeStep(JobRepository jobRepository, JraLeadingSireScrapeTasklet tasklet) {
+		return new StepBuilder("Scrape", jobRepository).tasklet(tasklet).build();
 	}
 
 	@Bean
-	public Step scrape2SaiStep(JobRepository jobRepository, PlatformTransactionManager transactionManager,
-			JraLeading2SaiSireScrapeTasklet tasklet) {
-		return new StepBuilder("Scrape2Sai", jobRepository).tasklet(tasklet, transactionManager).build();
+	public Step scrape2SaiStep(JobRepository jobRepository, JraLeading2SaiSireScrapeTasklet tasklet) {
+		return new StepBuilder("Scrape2Sai", jobRepository).tasklet(tasklet).build();
 	}
 
 	@Bean
